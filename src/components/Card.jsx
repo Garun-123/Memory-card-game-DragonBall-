235c6e28446d7f58f0f684611bdf31bec9e79cf7
GarunJad <garunjadaun@gmail.com>
2025-07-28 03:47:37 +0530

    Add functionality for reducing the clicked attribute of card to false if they are clicked more than once

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,25 @@
 import {useEffect,useState} from 'react';
 import  '../style/Card.css';
 
-function Card({setCount,num})
+function Card({setCount,num,setCard})
 {
-    const[card,setCard]=useState(false);
     const[imgdata,setImgData]=useState(null);
     
-    function checkclick()
-    {
-        
-        if(card===true)
-        {
-            setCount(0);
-            
-        }
-        else
-        {
-            setCount(prev=>prev+1);
-            setCard(true);
-        }
+      function handleCardClick(id) {
+    if (clicked === true) {
+      setCount(0);
+      setCard(prev =>
+        prev.map(item => ({ ...item, clicked: false }))
+      );
+    } else {
+      setCount(prev => prev + 1);
+      setCard(prev =>
+        prev.map(item =>
+          item.id === id ? { ...item, clicked: true } : item
+        )
+      );
     }
+  }
 
     
     useEffect(()=>{
@@ -40,7 +40,7 @@ function Card({setCount,num})
         <div className="card">
             <div className="img" >
        {imgdata?.image? (<img src={imgdata.image} alt={imgdata.name} onClick={()=>{
-       checkclick();
+       handleCardClick(num);      
        }}/>)
                  :(<p>Loading....</p>)}
            </div>
