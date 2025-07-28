21348aeaad2d21f56555419bb15027b48c7def56
GarunJad <garunjadaun@gmail.com>
2025-07-28 14:25:53 +0530

    Remove the the rendering from the Card.jsx as it is taking time while fetching and the clicked attribute is of older state which causes inaccuracy

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,48 +1,15 @@
-import {useEffect,useState} from 'react';
 import  '../style/Card.css';
 
-function Card({setCount,num,setCard})
+function Card({imgdata,onCardClick})
 {
-    const[imgdata,setImgData]=useState(null);
-    
-      function handleCardClick(id) {
-    if (clicked === true) {
-      setCount(0);
-      setCard(prev =>
-        prev.map(item => ({ ...item, clicked: false }))
-      );
-    } else {
-      setCount(prev => prev + 1);
-      setCard(prev =>
-        prev.map(item =>
-          item.id === id ? { ...item, clicked: true } : item
-        )
-      );
-    }
-  }
-
-    
-    useEffect(()=>{
-        const controller=new AbortController();
-        fetch(`https://dragonball-api.com/api/characters/${num}`, {
-        signal: controller.signal,
-                    })
-                    .then((res)=>res.json())
-                    .then((data)=>setImgData(data))
-                    .catch(err=>console.error(err));
-        return()=>{
-            controller.abort();
-        };
-    },[num])
     
     return (
         <>
-        <div className="card">
+        <div className="card"  onClick={()=>{
+        onCardClick(imgdata.id)}} >
             <div className="img" >
-       {imgdata?.image? (<img src={imgdata.image} alt={imgdata.name} onClick={()=>{
-       handleCardClick(num);      
-       }}/>)
-                 :(<p>Loading....</p>)}
+                {imgdata?.image? (<img src={imgdata.image} alt={imgdata.name}/>):
+                (<p>Loading....</p>)}
            </div>
         <div className="name"><p>{imgdata? imgdata.name:"Loading...."}</p></div>
         </div>
